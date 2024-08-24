1416aea405206876350809e3ca442cbc9cc5ae56
turk <turk@ddbg.io>
2024-08-24 16:42:01 -0600

    setup basic go backend, logging, and embed

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"path"
+)
+
+const embedDir = "temp"
+
+//go:embed temp
+var content embed.FS // object representing the embedded directory
+
+func main() {
+	// setup logger
+	opts := slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+	handler := slog.NewJSONHandler(os.Stderr, &opts)
+	log := slog.New(handler)
+
+	// read embed dir
+	fs, err := content.ReadDir(embedDir)
+	if err != nil {
+		log.Error("failed to read directory",
+			"error", err,
+		)
+		panic(err)
+	}
+
+	// log found files in embed dir
+	for _, f := range fs {
+		// read that file
+		path := path.Join(embedDir, f.Name())
+		fileContents, err := content.ReadFile(path)
+		if err != nil {
+			log.Error("failed to read file",
+				"error", err,
+				"file", f.Name(),
+			)
+			continue
+		}
+		log.Debug("file found",
+			"file", f.Name(),
+			"contents", string(fileContents),
+		)
+	}
+
+	// start server
+	port := 8008
+	log.Info("starting server",
+		"port", port,
+	)
+	fmt.Println("Server is running on port", port)
+	http.HandleFunc("/", serveRoot)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		slog.Error("failed to start server",
+			"error", err,
+			"port", port,
+		)
+	}
+	log.Info("server stopped", "port", port)
+}
+
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
