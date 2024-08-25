b1a6ae49264046b0ccecfde43941ae213242ed23
turk <turk@ddbg.io>
2024-08-24 23:27:21 -0600

    add sqlc for db

---
+++ inventory_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTODO(t *testing.T) {
+	t.Log("TODO: write tests")
+	t.Fail()
+}
---
+++ sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE foo (
+  id INT PRIMARY KEY,
+  name VARCHAR(255)
+);
